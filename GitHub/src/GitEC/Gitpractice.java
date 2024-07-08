f2a3975e0bb268574a8918b19bdab0b791b21adc
Suumit <suumitphadkay556@gmail.com>
2024-07-08 10:29:31 +0530

    Done by Suumit

---
+++ GitHub/src/GitEC/Gitpractice.java
@@ -0,0 +1,13 @@
+package GitEC;
+
+public class Gitpractice {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("This is method");
+	}
+		public void m1()
+		{
+			System.out.println("This is m1");
+		}
+	}
