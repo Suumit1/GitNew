960d8079391aac071c64e4f28f699b985e560312
Suumit1 <159883593+Suumit1@users.noreply.github.com>
2024-07-08 10:32:17 +0530

    Update Gitpractice.java
    New Edit from Suumit

--- GitHub/src/GitEC/Gitpractice.java
+++ GitHub/src/GitEC/Gitpractice.java
@@ -3,7 +3,9 @@ package GitEC;
 public class Gitpractice {
 
 	public static void main(String[] args) 
-	{
+	{       
+		GitPractice p=new GitPractice;
+		p.m1()
 		System.out.println("This is method");
 	}
 		public void m1()
